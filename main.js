95d6db3781dccd544927f88ef4ba6fbedac8da12
PapithaSekar <papithasekar88@gmail.com>
2019-02-02 10:28:22 +0100

    Modified variable names in test.js

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ module.exports = {getCardValue,winnerOfDeck1,getHandValue};/*specify the functio
     spades: colorInBlack('S'),
     hearts: colorInRed('H'),
     diamonds: colorInRed('D')
-
 }
-
 module.exports.SYMBOLS = SYMBOLS;*/
 
 //function for setting card values
@@ -18,69 +16,53 @@ function getCardValue(card) {
 
     console.log("[DEBUG] card in hand is " + rank+ " of " + symbol);
 
-   switch(rank)
-   {
-       case 'A':
-           return 11;
+    switch(rank)
+    {
+        case 'A':
+            return 11;
 
-       case '2':
-           return 2;
+        case '2':
+            return 2;
 
-       case '3':
+        case '3':
+            return 3;
 
-           return 3;
-       case '4':
+        case '4':
+            return 4;
 
-           return 4;
-       case '5':
+        case '5':
+            return 5;
 
-           return 5;
-       case '6':
+        case '6':
+            return 6;
 
-           return 6;
-       case '7':
+        case '7':
+            return 7;
 
-           return 7;
-       case '8':
+        case '8':
+            return 8;
 
-           return 8;
-       case '9':
+        case '9':
+            return 9;
 
-           return 9;
-       case '10':
-
-           return 10;
-       case 'J':
-
-           return 10;
-       case 'Q':
-
-           return 10;
-        case 'K':
-
+        case '10':
             return 10;
-   }
 
-    return name;
-   console.log(name);
+        case 'J':
+            return 10;
+
+        case 'Q':
+            return 10;
+
+        case 'K':
+            return 10;
+    }
+
+    return rank;
+
 }
 
-function winnerOfDeck1(p1,p2){
-
-   let valueOfHandP1 = getHandValue(p1.cards);
-   let valueOfHandP2 = getHandValue(p2.cards);
-
-   if((valueOfHandP1 == 21) && (valueOfHandP2 == 21) ){
-
-       console.log("p1 wins");
-       return p1;
-
-   }
-    else{
-        console.log("p2 wins");
-        return p2;
-   }
-}
+//function to check the values in hand
 
 function getHandValue(cards){
     let value = 0;
@@ -92,5 +74,24 @@ function getHandValue(cards){
 
     return value;
 }
-// function to pick random cards
 
+//function to check who is the winner in round1
+
+function winnerOfDeck1(p1,p2){
+
+    let valueOfHandP1 = getHandValue(p1.cards);
+    let valueOfHandP2 = getHandValue(p2.cards);
+
+    if((valueOfHandP1 == 21) && (valueOfHandP2 == 21) ){
+
+        console.log("p1 wins");
+        return p1;
+
+    }
+    else{
+        console.log("p2 wins");
+        return p2;
+    }
+}
+
+// function to pick random cards
--- test.js
+++ test.js
@@ -1,41 +1,42 @@
 const featuresToTest = require('../BJ/main');
 
-const aceofclubs = {symbol: "C", rank: "A"};
-const jackofspade = {symbol: "S", rank: "J"};
-const fourofdiamond = {symbol: "D", rank: "4"};
-const tenofheart = {symbol: "H", rank: "10"};
-const ACE_OF_HEARTS = {symbol: "H", rank: "A"};
+const ACE_OF_CLUB = {symbol: "C", rank: "A"};
+const JACK_OF_SPADE = {symbol: "S", rank: "J"};
+const FOUR_OF_DIAMOND = {symbol: "D", rank: "4"};
+const TEN_OF_HEART = {symbol: "H", rank: "10"};
+const ACE_OF_HEART = {symbol: "H", rank: "A"};
 
 // check the value of the card
 
 test(' check the values of the card',() => {
 
-    expect(featuresToTest.getCardValue(aceofclubs)).toBe(11);
-    expect(featuresToTest.getCardValue(jackofspade)).toBe(10);
-    expect(featuresToTest.getCardValue(fourofdiamond)).toBe(4);
-    expect(featuresToTest.getCardValue(tenofheart)).toBe(10);
+    expect(featuresToTest.getCardValue(ACE_OF_CLUB)).toBe(11);
+    expect(featuresToTest.getCardValue(JACK_OF_SPADE)).toBe(10);
+    expect(featuresToTest.getCardValue(FOUR_OF_DIAMOND)).toBe(4);
+    expect(featuresToTest.getCardValue(TEN_OF_HEART)).toBe(10);
 });
 
+//test2 to check values in hand
 test(' check the values of a hand',() => {
-    expect(featuresToTest.getHandValue([aceofclubs, jackofspade])).toBe(21);
-    expect(featuresToTest.getHandValue([aceofclubs, jackofspade, ACE_OF_HEARTS])).toBe(32);
+    expect(featuresToTest.getHandValue([ACE_OF_CLUB, JACK_OF_SPADE])).toBe(21);
+    expect(featuresToTest.getHandValue([ACE_OF_CLUB, JACK_OF_SPADE, ACE_OF_HEART])).toBe(32);
 
 
 
 });
 
-//test2 to check who is the winner in first deck
+//test3  to check who is the winner in first deck
 
 test(' check who is the winner in first deck',() => {
 
     let p1 = {
         name:  'sam',
-        cards:  [ aceofclubs,tenofheart],
+        cards:  [ ACE_OF_CLUB,TEN_OF_HEART],
     };
 
     let p2 = {
         name: 'dealer',
-        cards:  [ ACE_OF_HEARTS,jackofspade],
+        cards:  [ ACE_OF_HEART,JACK_OF_SPADE],
     };
 
     let winner = featuresToTest.winnerOfDeck1(p1,p2);
